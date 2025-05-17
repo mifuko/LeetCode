71c527a77264be26fe806e2885b6001f384e09f5
Mifuko <20350047+mifuko@users.noreply.github.com>
2025-05-17 10:46:05 +0900

    LC-combination-sum [Runtime: N/A; Memory: N/A]

---
+++ array/combination-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        list a = []
+        if target < candidates[]
+            return list a
+        
+        
