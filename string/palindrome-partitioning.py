0ca2f687772055c5090a114fbd5b3de549ea71e4
Mifuko <20350047+mifuko@users.noreply.github.com>
2025-05-20 19:13:15 +0900

    LC-palindrome-partitioning [Runtime: 30 ms; Memory: 34.7 MB]

--- string/palindrome-partitioning.py
+++ string/palindrome-partitioning.py
@@ -1,19 +1,23 @@
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
+        n = len(s)
         res = []
+        dp = [[False]*n for _ in range(n)]
 
-        def is_palindrome(sub: str) -> bool:
-            return sub == sub[::-1]
+        # 预处理回文子串
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i < 3 or dp[i+1][j-1]):
+                    dp[i][j] = True
 
         def backtrack(start: int, path: list[str]):
-            if start == len(s):
+            if start == n:
                 res.append(path[:])
                 return
-            for end in range(start + 1, len(s) + 1):
-                substring = s[start:end]
-                if is_palindrome(substring):
-                    path.append(substring)
-                    backtrack(end, path)
+            for end in range(start, n):
+                if dp[start][end]:
+                    path.append(s[start:end+1])
+                    backtrack(end+1, path)
                     path.pop()
 
         backtrack(0, [])
