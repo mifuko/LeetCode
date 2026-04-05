8d5ca015af5e377b3d0d6679eba9304f9923713b
Mifuko <20350047+mifuko@users.noreply.github.com>
2026-04-06 08:41:06 +0900

    LC-robot-return-to-origin [Runtime: 15 ms; Memory: 19.2 MB]

--- string/robot-return-to-origin.py
+++ string/robot-return-to-origin.py
@@ -1,4 +1,16 @@
+# class Solution:
+#     def judgeCircle(self, moves: str) -> bool:
+#         return moves.count('L') == moves.count('R') and \
+#             moves.count('U') == moves.count('D')
+
+
+### 普通版
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        return moves.count('L') == moves.count('R') and \
-            moves.count('U') == moves.count('D')
+        x = y = 0
+        for move in moves:
+            if move == 'R': x += 1
+            elif move == 'L': x -= 1
+            elif move == 'U': y += 1
+            elif move == 'D': y -= 1
+        return x == 0 and y == 0
